da7503a4b4490b0d91cf3c9661e4fff13b783181
mihai <stoenica.mihai2016@gmail.com>
2025-12-06 20:26:07 +0200

    removed security

--- src/main/java/com/soil_humidity_api/SecurityConfig.java
+++ src/main/java/com/soil_humidity_api/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.soil_humidity_api;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,6 +12,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) {
 
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
 }
