abee41d3e8a6508944204656895899a9e25c143a
mihai <stoenica.mihai2016@gmail.com>
2025-12-06 10:36:58 +0200

    first commit

--- src/main/java/com/soil_hunidity_api/SecurityConfig.java
+++ src/main/java/com/soil_hunidity_api/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.soil_hunidity_api;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
