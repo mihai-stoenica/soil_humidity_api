04576ab54036dffcd0c968fc35df32f6256f0fae
mihai <stoenica.mihai2016@gmail.com>
2025-12-08 21:30:07 +0200

    test without cors

--- src/main/java/com/soil_humidity_api/config/SecurityConfig.java
+++ src/main/java/com/soil_humidity_api/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
         //configuration.setAllowedOrigins(List.of(allowedOrigins.split(",")));
 
         configuration.setAllowedOriginPatterns(List.of(
-                "http://192.168.*.*",
+                "ws://192.168.*.*",
                 "http://10.*.*.*",
                 "http://172.16.*.*",
                 "http://localhost:*"
