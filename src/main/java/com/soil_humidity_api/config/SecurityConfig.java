784eb02579e584e70c637554d4c77ca380181205
mihai <stoenica.mihai2016@gmail.com>
2025-12-08 20:39:03 +0200

    test without cors

--- src/main/java/com/soil_humidity_api/config/SecurityConfig.java
+++ src/main/java/com/soil_humidity_api/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
         http
                 //.cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()
+                .authorizeHttpRequests(auth -> auth. requestMatchers("/api/auth/login", "/api/auth/register", "/api/auth/logout").permitAll(). anyRequest().authenticated()
                 )
                 .addFilterBefore(apiKeyFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(jwtCookieFilter, UsernamePasswordAuthenticationFilter.class);
