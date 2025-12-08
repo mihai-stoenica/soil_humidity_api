f1cf11e6efd56a43b64dc56f64314a00bb565479
mihai <stoenica.mihai2016@gmail.com>
2025-12-08 19:37:43 +0200

    debug

--- src/main/java/com/soil_humidity_api/config/SecurityConfig.java
+++ src/main/java/com/soil_humidity_api/config/SecurityConfig.java
@@ -29,9 +29,6 @@ public class SecurityConfig {
         http
                 .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
-                .exceptionHandling(ex -> ex.authenticationEntryPoint(
-                        (request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
-                ))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/login", "/api/auth/register", "/api/auth/logout", "/ws/**").permitAll()
                         .anyRequest().authenticated()
