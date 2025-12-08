cb012d85615eaaa255daadc40b1f6cd4a840d7f6
mihai <stoenica.mihai2016@gmail.com>
2025-12-08 14:12:52 +0200

    fixed bugs

--- src/main/java/com/soil_humidity_api/config/SecurityConfig.java
+++ src/main/java/com/soil_humidity_api/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
-        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-API-KEY"));
+        configuration.setAllowedHeaders(List.of("*"));
 
         configuration.setAllowCredentials(true);
 
--- src/main/java/com/soil_humidity_api/handler/SoilSocketHandler.java
+++ src/main/java/com/soil_humidity_api/handler/SoilSocketHandler.java
@@ -3,11 +3,9 @@ package com.soil_humidity_api.handler;
 import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
-import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
