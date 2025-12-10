b5ee5e7486c0e5a3104e6ebdf8badde60b5bf340
mihai <stoenica.mihai2016@gmail.com>
2025-12-11 00:44:20 +0200

    test with react

--- src/main/java/com/soil_humidity_api/config/WebSocketConfig.java
+++ src/main/java/com/soil_humidity_api/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.soil_humidity_api.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -20,7 +21,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/stomp-ws")
-                .setAllowedOrigins("*")
+                .setAllowedOriginPatterns("*")
                 .withSockJS(); // SockJS adds compatibility, but is optional
     }
 }
--- src/main/java/com/soil_humidity_api/controller/TesterController.java
+++ src/main/java/com/soil_humidity_api/controller/TesterController.java
@@ -8,6 +8,39 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class TesterController {
 
+    static public class MessagePayloadDTO {
+
+        private Long messageId; // Matches 'messageId' (you're sending Date.now())
+        private String data;    // Matches 'data'
+
+        // You must include standard getters and setters (or use Lombok)
+        // for Jackson to work!
+
+        public Long getMessageId() {
+            return messageId;
+        }
+
+        public void setMessageId(Long messageId) {
+            this.messageId = messageId;
+        }
+
+        public String getData() {
+            return data;
+        }
+
+        public void setData(String data) {
+            this.data = data;
+        }
+
+        @Override
+        public String toString() {
+            return "MessagePayloadDTO{" +
+                    "messageId=" + messageId +
+                    ", data='" + data + '\'' +
+                    '}';
+        }
+    }
+
     private final SimpMessagingTemplate messagingTemplate;
 
     public TesterController(SimpMessagingTemplate messagingTemplate) {
@@ -15,16 +48,17 @@ public class TesterController {
     }
 
     @MessageMapping("/send-data")
-    public void receiveMessageAndRespond(String receivedData) {
+    // Change the parameter type from String to your new DTO
+    public void receiveMessageAndRespond(MessagePayloadDTO receivedPayload) {
 
-        System.out.println("Received data from client: " + receivedData);
+        System.out.println("Received data from client: " + receivedPayload.toString());
 
-        // 1. Log the receipt
-        String logMessage = "Server received: " + receivedData;
+        // You can now access fields directly:
+        // Long id = receivedPayload.getMessageId();
+        // String text = receivedPayload.getData();
 
-        // 2. Send a confirmation message back to all subscribed clients on a public topic
-        // The client must subscribe to /topic/response to see this.
-        String response = "SERVER ACK: Received your message: '" + receivedData + "'";
+        // 2. Send a confirmation message back...
+        String response = "SERVER ACK: Received ID: " + receivedPayload.getMessageId();
 
         messagingTemplate.convertAndSend("/topic/response", response);
     }
