4338fd55f16f84f94e1731b02c84f239aab473b5
mihai <stoenica.mihai2016@gmail.com>
2025-12-11 10:58:00 +0200

    debug

--- src/main/java/com/soil_humidity_api/config/WebSocketConfig.java
+++ src/main/java/com/soil_humidity_api/config/WebSocketConfig.java
@@ -21,7 +21,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/stomp-ws")
-                .setAllowedOriginPatterns("*")
-                .withSockJS(); // SockJS adds compatibility, but is optional
+                .setAllowedOriginPatterns("*");
     }
 }
